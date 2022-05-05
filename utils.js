6ac53804dbe1b81ff710d2de2f2b8af6f38b0b9d
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-05 12:52:33 -0700

    adding render function

--- utils.js
+++ utils.js
@@ -2,6 +2,15 @@ export function renderCandy(candy) {
     const div = document.createElement('div');
     div.classList.add('candy');
 
+    const h1 = document.createElement('h1');
+    h1.textContent = candy.name;
+    
+    const img = document.createElement('img');
+    img.textContent = candy.name;
+
+
+
+
     div.append(div);
     return div;
 }
